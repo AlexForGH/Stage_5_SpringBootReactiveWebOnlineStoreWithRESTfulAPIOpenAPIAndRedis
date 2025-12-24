51e88c10cbc020e4fabe1567f4d21cbaf27b3221
AlexForGH <for_git_hub@mail.ru>
2025-12-24 16:05:04 +0300

    add open api config in web-store-service andr edit config payment-service

--- restful-payment-service/src/main/java/org/pl/controller/UserBalanceController.java
+++ restful-payment-service/src/main/java/org/pl/controller/UserBalanceController.java
@@ -1,7 +1,8 @@
 package org.pl.controller;
 
-import org.pl.model.BalanceResponse;
-import org.pl.model.BalanceUpdateRequest;
+
+import org.pl.payment_service.server.payment.model.BalanceResponse;
+import org.pl.payment_service.server.payment.model.BalanceUpdateRequest;
 import org.pl.service.UserBalanceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
 @RestController
-class UserBalanceController implements org.pl.api.DefaultApi {
+class UserBalanceController implements org.pl.payment_service.server.payment.api.DefaultApi {
 
     private final UserBalanceService userBalanceService;
 
